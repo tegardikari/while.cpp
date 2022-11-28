8c58b2ec5a5931a28c5d2b2038af4520b0b3b46b
tegardikari <111736182+tegardikari@users.noreply.github.com>
2022-11-28 20:56:58 +0700

    Add files via upload

---
+++ while.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+//Compiler version g++ 6.3.0
+
+int main()
+{
+   for(int x=1;x <=1000;x++){
+     cout<<x<<endl;
+     cout<<"hello word"<<endl;
+   }
+   
+    int y = 1;
+    while(y <=1000)
+      printf ("loop ke %i\n",y);
+      y++;
+    }
